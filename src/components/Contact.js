6ab75c74ce39233dba815667fc3b76bda5bffd6b
kmehkeri <kmehkeri@gmail.com>
2018-09-24 13:41:51 +0200

    Controlled contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,44 +1,157 @@
-import React from 'react';
-import { Breadcrumb, BreadcrumbItem, Container, Col, Row } from 'reactstrap'; 
+import React, { Component } from 'react';
+import { Breadcrumb, BreadcrumbItem, Button, Container, Col, Form, FormGroup, Input, Label, Row } from 'reactstrap'; 
 import { Link } from 'react-router-dom';
 
-function Contact(props) {
-    return(
-        <Container>
-            <Row>
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>Contact</BreadcrumbItem>
-                </Breadcrumb>
-            </Row>
-            <Row>
-                <Col xs="12">
-                    <h3>Location Information</h3>
-                </Col>
-                <Col sm={{size: 4, offset: 1}}>
-                        <h5>Our Address</h5>
-                        <address>
-                            121, Dark Pollution Bay Road<br />
-                            Temple of Doom, Arkham<br />
-                            SOMEWHERE<br />
-                            <i className="fa fa-phone"></i>: +852 1234 5678<br />
-                            <i className="fa fa-fax"></i>: +852 8765 4321<br />
-                            <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">contact@cthulhurestaurant.net</a>
-                        </address>
-                </Col>
-                <Col sm={{size: 6, offset: 1}}>
-                    <h5>Map of our Location</h5>
-                </Col>
-                <Col sm={{size: 11, offset: 1}}>
-                    <div className="btn-group" role="group">
-                        <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
-                        <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
-                        <a role="button" className="btn btn-success" href="mailto:contact@cthulhurestaurant.net"><i className="fa fa-envelope-o"></i> Email</a>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
-    );
+class Contact extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstname: '',
+            lastname: '',
+            tel: '',
+            email: '',
+            agree: false,
+            contactType: 'Tel',
+            message: ''
+        }
+    }
+
+    handleInputChange = (event) => {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit = (event) => {
+        console.log("Current state is: " + JSON.stringify(this.state));
+        alert("Current state is: " + JSON.stringify(this.state));
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Contact</BreadcrumbItem>
+                    </Breadcrumb>
+                </Row>
+                <Row>
+                    <Col xs="12">
+                        <h3>Location Information</h3>
+                    </Col>
+                    <Col sm={{size: 4, offset: 1}}>
+                            <h5>Our Address</h5>
+                            <address>
+                                121, Dark Pollution Bay Road<br />
+                                Temple of Doom, Arkham<br />
+                                SOMEWHERE<br />
+                                <i className="fa fa-phone"></i>: +852 1234 5678<br />
+                                <i className="fa fa-fax"></i>: +852 8765 4321<br />
+                                <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">contact@cthulhurestaurant.net</a>
+                            </address>
+                    </Col>
+                    <Col sm={{size: 6, offset: 1}}>
+                        <h5>Map of our Location</h5>
+                    </Col>
+                    <Col sm={{size: 11, offset: 1}}>
+                        <div className="btn-group" role="group">
+                            <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
+                            <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
+                            <a role="button" className="btn btn-success" href="mailto:contact@cthulhurestaurant.net"><i className="fa fa-envelope-o"></i> Email</a>
+                        </div>
+                    </Col>
+                </Row>
+                <Row className="mt-4">
+                    <Col xs="12">
+                        <h3>Send us your feedback!</h3>
+                    </Col>
+                    <Col xs="12" md="9">
+                        <Form onSubmit={this.handleSubmit}>
+                        <FormGroup row>
+                                <Label htmlFor="firstname" md={2}>First Name</Label>
+                                <Col md={10}>
+                                    <Input type="text" id="firstname" name="firstname"
+                                        placeholder="First Name"
+                                        value={this.state.firstname}
+                                        onChange={this.handleInputChange} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="lastname" md={2}>Last Name</Label>
+                                <Col md={10}>
+                                    <Input type="text" id="lastname" name="lastname"
+                                        placeholder="Last Name"
+                                        value={this.state.lastname}
+                                        onChange={this.handleInputChange} />
+                                </Col>                        
+                            </FormGroup>
+                            <FormGroup row>
+                            <Label htmlFor="telnum" md={2}>Contact Tel.</Label>
+                                <Col md={10}>
+                                    <Input type="tel" id="telnum" name="telnum"
+                                        placeholder="Tel. number"
+                                        value={this.state.telnum}
+                                        onChange={this.handleInputChange} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="email" md={2}>Email</Label>
+                                <Col md={10}>
+                                    <Input type="email" id="email" name="email"
+                                        placeholder="Email"
+                                        value={this.state.email}
+                                        onChange={this.handleInputChange} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Col md={{size: 6, offset: 2}}>
+                                    <FormGroup check>
+                                        <Label check>
+                                            <Input type="checkbox"
+                                                name="agree"
+                                                checked={this.state.agree}
+                                                onChange={this.handleInputChange} /> {' '}
+                                            <strong>May we contact you?</strong>
+                                        </Label>
+                                    </FormGroup>
+                                </Col>
+                                <Col md={{size: 3, offset: 1}}>
+                                    <Input type="select" name="contactType"
+                                            value={this.state.contactType}
+                                            onChange={this.handleInputChange}>
+                                        <option>Tel</option>
+                                        <option>Email</option>
+                                    </Input>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="message" md={2}>Your Feedback</Label>
+                                <Col md={10}>
+                                    <Input type="textarea" id="message" name="message"
+                                        rows="12"
+                                        value={this.state.message}
+                                        onChange={this.handleInputChange}></Input>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Col md={{size: 10, offset: 2}}>
+                                    <Button type="submit" color="primary">
+                                        Send Feedback
+                                    </Button>
+                                </Col>
+                            </FormGroup>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
 }
 
 export default Contact;
