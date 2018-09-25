58cb5bffe1a8d7874ee04cb2bf3601cdd53b707e
kmehkeri <kmehkeri@gmail.com>
2018-09-25 12:10:34 +0200

    Uncontrolled login form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import { Col, Collapse, Container, Jumbotron, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, Row } from 'reactstrap';
+import { Button, Col, Collapse, Container, Form, FormGroup, Input, Jumbotron, Label, Modal, ModalHeader, ModalBody, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, Row } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
@@ -7,7 +7,8 @@ class Header extends Component {
         super(props)
 
         this.state = {
-            isNavOpen: false
+            isNavOpen: false,
+            isLoginOpen: false
         }
     }
 
@@ -15,9 +16,45 @@ class Header extends Component {
         this.setState({ isNavOpen: !this.state.isNavOpen })
     }
 
+    toggleLogin = () => {
+        this.setState({ isLoginOpen: !this.state.isLoginOpen })
+    }
+
+    handleLogin = (event) => {
+        this.toggleLogin();
+        alert("Username: " + this.username.value + ", password: " + this.password.value + ", remember: " + this.remember.checked);
+        event.preventDefault();
+    }
+
     render() {
         return (
             <Fragment>
+                <Modal isOpen={this.state.isLoginOpen} toggle={this.toggleLogin}>
+                    <ModalHeader toggle={this.toggleLogin}>Login</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                    innerRef={(input) => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                    innerRef={(input) => this.password = input}  />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember"
+                                    innerRef={(input) => this.remember = input}  />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
+
                 <Navbar dark expand="md" color="primary">
                     <Container>
                         <NavbarToggler onClick={this.toggleNav} />
@@ -43,6 +80,13 @@ class Header extends Component {
                                     <NavLink className="nav-link" to="/contact">
                                         <span className="fa fa-address-card fa-lg"></span> Contact
                                     </NavLink>
+                                </NavItem>
+                            </Nav>
+                            <Nav navbar className="ml-auto">
+                                <NavItem>
+                                    <Button color="primary" onClick={this.toggleLogin}>
+                                        <span className="fa fa-sign-in fa-lg"></span> Login
+                                    </Button>
                                 </NavItem>
                             </Nav>
                         </Collapse>
