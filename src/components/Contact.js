96aa86d87d3275d2b19b787a1ca701889c1e4561
kmehkeri <kmehkeri@gmail.com>
2018-09-26 13:12:30 +0200

    Refactor contact form to use shared validators and messages

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,7 @@ import React, { Component } from 'react';
 import { Breadcrumb, BreadcrumbItem, Button, Container, Col, FormGroup, Label, Row } from 'reactstrap'; 
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
-
-const required = (val) => val && val.length;
-const maxLength = (len) => (val) => !val || val.length <= len;
-const minLength = (len) => (val) => !val || val.length >= len;
-const isNumber = (val) => !val || !isNaN(Number(val));
-const isValidEmail = (val) => !val || /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}/i.test(val);
+import { required, minLength, maxLength, isNumber, isValidEmail, messages } from '../shared/validations';
 
 class Contact extends Component {
     handleSubmit = (values) => {
@@ -62,7 +57,7 @@ class Contact extends Component {
                                     <Control.text model=".firstname" id="firstname" name="firstname" placeholder="First Name" className="form-control"
                                         validators={{ required, minLength: minLength(3), maxLength: maxLength(15) }} />
                                     <Errors className="text-danger" model=".firstname" show="touched"
-                                        messages={{ required: 'Required', minLength: 'Minimum 2 characters', maxLength: '15 characters or less' }} />
+                                        messages={messages} />
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -71,7 +66,7 @@ class Contact extends Component {
                                     <Control.text model=".lastname" id="lastname" name="lastname" placeholder="Last Name" className="form-control"
                                         validators={{ required, minLength: minLength(3), maxLength: maxLength(15) }} />
                                     <Errors className="text-danger" model=".lastname" show="touched"
-                                        messages={{ required: 'Required', minLength: 'Minimum 2 characters', maxLength: '15 characters or less' }} />
+                                        messages={messages} />
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -80,7 +75,7 @@ class Contact extends Component {
                                     <Control.text model=".telnum" id="telnum" name="telnum" placeholder="Tel. Number" className="form-control"
                                         validators={{ required, minLength: minLength(3), maxLength: maxLength(15), isNumber }} />
                                     <Errors className="text-danger" model=".telnum" show="touched"
-                                        messages={{ required: 'Required', minLength: 'Minimum 2 characters', maxLength: '15 characters or less', isNumber: 'Must be a number' }} />
+                                        messages={messages} />
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -89,7 +84,7 @@ class Contact extends Component {
                                     <Control.text model=".email" id="email" name="email" placeholder="Email" className="form-control"
                                         validators={{ required, maxLength: maxLength(100), isValidEmail }} />
                                     <Errors className="text-danger" model=".email" show="touched"
-                                        messages={{ required: 'Required', maxLength: '100 characters or less', isValidEmail: 'Must be a valid email' }} />
+                                        messages={messages} />
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
